58021b5b98e8effe643cc29ebc263e9f748134b4
dburrigh <dburrigh@nebrwesleyan.edu>
2018-04-17 15:41:18 -0500

    In progress

---
+++ Student Work/dburrigh/src/Problem1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Problem1 {
+
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        String Sentence = scan.nextLine();
+        String[] words;
+
+        System.out.println("+" + )
+    }
+
+    public static int LongestWord(String word)
+    {
+        for(int i = 0; i < words.length; i++)
+        {
+            
+        }
+    }
+}
