6a5e84ab6fa17622c5da3f7e10f1801760e1e712
bwortman2 <35316840+bwortman2@users.noreply.github.com>
2018-04-27 16:11:27 -0500

    Add files via upload

---
+++ Student Work/BenWortmanHW2/HW2_20.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class HW2_20 {
+
+    private static void main(String[] args)
+    {
+
+    Scanner num = new Scanner(System.in);
+
+    String entered = num.nextLine();
+
+    int number = entered.length();
+
+    int[] arr = new int[number];
+    }
+
+
+}
