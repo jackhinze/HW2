e2c3caa8230e5e36ce607f606868a875527e6aad
bwortman2 <35316840+bwortman2@users.noreply.github.com>
2018-04-27 18:54:19 -0500

    Update HW2_1.java

--- Student Work/BenWortmanHW2/HW2_1.java
+++ Student Work/BenWortmanHW2/HW2_1.java
@@ -28,47 +28,7 @@ public class HW2_1
 
         }
         return MostCharacters;
-    }
-
-    private static void entered(String[] number)
-    {
-
-        System.out.print("+");
-
-        int e = HW2_1.MostCharacters(number);
-
-        for (int n = 1; n < e + 3; n++)
-        {
-
-            System.out.print("-");
-        }
-        for (int n = 0; n < number.length; n++)
-        {
-
-            System.out.print("| " + number[n]);
-
-            for (int b = 0; b <= e - number[n].length(); b++)
-            {
-
-                System.out.print(" ");
-            }
-            System.out.println("|");
-        }
-
-            System.out.println("+");
-
-
-                    System.out.print("+");
-
-            for (int n = 1; n < e + 3; n++)
-            {
-
-                System.out.print("-");
-            }
-                System.out.print("+");
-    }
-
-
+    }  
 }
 
 
